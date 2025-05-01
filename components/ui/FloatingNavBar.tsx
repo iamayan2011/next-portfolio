71366e8db2e0f26d97b0595efde6092a48e2cae9
Ayan <ayan.raza.2011@gmail.com>
2025-05-01 23:04:39 +0530

    more fixes

--- components/ui/FloatingNavBar.tsx
+++ components/ui/FloatingNavBar.tsx
@@ -9,6 +9,11 @@ import {
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
+type NavItem = {
+  name: string;
+  link: string;
+};
+
 export const FloatingNav = ({
   navItems,
   className,
@@ -60,7 +65,7 @@ export const FloatingNav = ({
           className
         )}
       >
-        {navItems.map((navItem: any, idx: number) => (
+        {navItems.map((navItem: NavItem, idx: number) => (
           <Link
             key={`link=${idx}`}
             href={navItem.link}
@@ -68,7 +73,7 @@ export const FloatingNav = ({
               "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
           >
-            <span className="block sm:hidden">{navItem.icon}</span>
+            
             <span className="hidden sm:block text-sm">{navItem.name}</span>
           </Link>
         ))}
